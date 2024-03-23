5365e789e2bc4b0e2b5fa992945e9c3aebd71e0f
ObvMamut <130008535+ObvMamut@users.noreply.github.com>
2024-03-23 12:30:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,9 +37,7 @@ def hierholzer(path, edges):
     return False
 
 
-path = [1] # Starting vertex  
-edges = [(1,2), (1,3), (2,3), (2,3), (2,5), (3,4), (4,6), (4,6), (4,7), (5,6), (6,8), (7,9), (8,9), (8,9), (8,11), (9,10), (10,12), (10,12), (10,13), (11,12), (12,13)] # Example of undirected graph, in this case a graph created from the logo of the Olympic Games
+path = [1]
+edges = [(1,2), (1,3), (2,3), (2,3), (2,5), (3,4), (4,6), (4,6), (4,7), (5,6), (6,8), (7,9), (8,9), (8,9), (8,11), (9,10), (10,12), (10,12), (10,13), (11,12), (12,13)]
 hierholzer(path, edges)
-
-# -- ObvMamut
 
